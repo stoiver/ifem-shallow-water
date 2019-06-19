54790ed1ff567af50192a7be28cc51cec63b6972
Stephen Roberts <stephen.roberts@anu.edu.au>
2019-06-19 17:14:34 +1000

    Running code

--- examples/shallow_water.m
+++ examples/shallow_water.m
@@ -1,3 +1,4 @@
+function shallow_water
 %% Shallow water Planar Dam Break
 %--------------------------------------------------------------------------
 % Sudi Mungkasi 2012, Australian National University
@@ -8,7 +9,7 @@
 close all; clear all;
 
 %---------------------- Parameters ----------------------------------------
-figure(1); set(gcf,'Units','normal'); set(gcf,'Position',[0,0,0.8,0.4]);
+%figure(1); set(gcf,'Units','normal'); set(gcf,'Position',[0,0,0.8,0.4]);
 
 t = 0;
 g = 9.81;
@@ -17,7 +18,7 @@ node = [-1 -1; 1 -1; 1 1; -1 1];
 elem = [2 3 1; 4 1 3];
 %bdFlag = setboundary(node,elem,'Dirichlet','all');
 
-for i = 1:3 % Try different level here. 7 is good.
+for i = 1:4 % Try different level here. 7 is good.
     [node,elem] = uniformbisect(node,elem);
 end
 
@@ -51,35 +52,54 @@ Q(:,4) = 0.5*h.*(u.^2+v.^2) + 0.5*g*h.^2; %entropy
 
 Area      = find_area(node,elem);
 tol_Area  = max(Area);
+init_res = tol_Area;
 
 fprintf('The initial number of elements is %d triangles.\n', nt);
 fprintf('The initial time is %d seconds.\n', t);
 
 
 % Plot the initial conditions
-figure(3)
-showmesh(node,elem);
-axis on;
-findelem(node,elem);  % plot indices of all triangles
-findnode(node);       % plot indices of all vertices
+% clf(figure(3));
+% figure(3);
+% showmesh(node,elem);
+% axis on;
+% findelem(node,elem);  % plot indices of all triangles
+% findnode(node);       % plot indices of all vertices
+
+
+clf(figure(1));
+figure(1);
+set(gcf,'Units','normal'); set(gcf,'Position',[0.8,0.05,0.15,0.15]);
+showmesh(node,elem); view(2); xlabel('x'); ylabel('y');
+
+
+clf(figure(2));
+figure(2);
+set(gcf,'Units','normal'); set(gcf,'Position',[0.8,0.3,0.15,0.15]);
+subplot(1,2,1);
+showsolution(node,elem,Q(:,1)); view(3); zlim([-1. 1.]);
+xlabel('x'); ylabel('y'); zlabel('h')
+subplot(1,2,2);
+showsolution(node,elem,Q(:,2)); view(3); zlim([-1. 1.]);
+xlabel('x'); ylabel('y'); zlabel('uh')
+
 
 %% MAIN PROGRAM STARTS HERE %%%%%%%%%%%
 
 maxRefineCoarsen = 2;
-maxIt = 2;
-dt = 0.002;
+maxIt = 0;
+dt = 0.001;
 
-for i = 1:maxIt
-    i
-        nt = size(elem,1); %number of triangles
-        
+
+while (t < 0.1) 
+    
+    nt = size(elem,1); %number of triangles
+    %fprintf('nt = %g \n',nt);
+    
+    for iter = 1:0
         % Test evolve; Used to decide on new mesh
-        [U, Area] = evolve_step(node,elem,Q,dt,g);
+        [U, Area, dt] = evolve_step(node,elem,Q,dt,g);
         
-        disp('Q U')
-        size(Q)
-        size(U)
-        size(Area)
         
         % Begin Refinement ----------------------------------------------------
         h  = U(:,1);
@@ -88,61 +108,75 @@ for i = 1:maxIt
         vh = U(:,3);
         v  = vh./h;
         Ent_after = 0.5*h.*(u.^2+v.^2) + 0.5*g*h.^2; %entropy
-        
-        NEP =(1/dt)*(Ent_after-U(:,4));%.*Area';
+        NEP = abs((1/dt)*(Ent_after-U(:,4)));%.*Area';
         
         tol_NEP = 0.25*max(abs(NEP));
-        refineElem  = find(abs(NEP) > tol_NEP);%0.000001*tol_NEP);
+        refineElem  = find((abs(NEP) > tol_NEP) .* (Area > init_res/16) );
         
-        Elems_to_be_refined = refineElem;
-        
-        for j = 1: size(refineElem,1)
-            if Area(refineElem(j))/tol_Area < 0.000001+1/(2^maxRefineCoarsen) %0.126 %1.1 %0.6 %0.26
-                Elems_to_be_refined = setdiff(Elems_to_be_refined, refineElem(j));
-            end
-        end
-        size(Elems_to_be_refined)
+        %     elems_to_be_refined = refineElem;
+        %
+        %     for j = 1: size(refineElem,2)
+        %         if Area(refineElem(1,j))/tol_Area < 0.000001+1/(2^maxRefineCoarsen) %0.126 %1.1 %0.6 %0.26
+        %             elems_to_be_refined = setdiff(elems_to_be_refined, refineElem(1,j));
+        %         end
+        %     end
+        %
+        %     refineElem = elems_to_be_refined;
         
         % Do the bisection of the domain
-        [node,elem,~,~,tree] = bisect(node,elem, Elems_to_be_refined);
-        ntn = size(elem,1)  %number of triangles after refinement
+        
+        %     fprintf('|refineElem| = %g \n',size(refineElem,1));
+        [node,elem,~,~,tree] = bisect(node,elem, refineElem);
+        
+        
+        %     clf(figure(6));
+        %     figure(6);
+        %     set(gcf,'Units','normal'); set(gcf,'Position',[0.6,0.25,0.15,0.15]);
+        %     showmesh(node,elem); view(2); xlabel('x'); ylabel('y');
+        
+        ntn = size(elem,1);  %number of triangles after refinement
+        %fprintf('refine ntn = %g \n',ntn);
         QR = zeros(ntn,4);
         for ii = 1:4
             QR(:,ii) = eleminterpolate(Q(:,ii),tree);
         end
         NEPR = eleminterpolate(NEP,tree);
-
+        
         % End Refinement ------------------------------------------------------
         
         
         % Begin Coarsening ----------------------------------------------------
         Area = find_area(node,elem);  %areas of triangles
+        tol_Area  = max(Area);
         coarsenElem = find(abs(NEPR) < tol_NEP);
-        Elems_to_be_coarsened = coarsenElem;
-        for j = 1: size(coarsenElem,1)
-            Area_j = Area(coarsenElem(j));
-            if Area_j/tol_Area >(2^maxRefineCoarsen)- 0.000001 %2.1%1.1 %0.9
-                Elems_to_be_coarsened = setdiff(Elems_to_be_coarsened, Area_j);
-            end
-        end
         
-        markedElem = Elems_to_be_coarsened;
-        % Do coarsening and update quatities at the same time
-        bdEdge = []; % This empty set bdEdge will return nothing.
         
-        %[node,elem,bdEdge,brother,QC,NEPC] = coarsen_sudi(node,elem, markedElem, bdEdge, QR, NEPR);
-        [node,elem,~,~,tree] = coarsen(node,elem, markedElem, bdEdge);
+        %fprintf('|coarsenElem| = %g \n',size(coarsenElem,1));
+        [node,elem,~,~,tree] = coarsen(node,elem, coarsenElem, []);
         
-        ntn = size(elem,1)  %number of triangles after refinement
+        %     clf(figure(5));
+        %     figure(5);
+        %     set(gcf,'Units','normal'); set(gcf,'Position',[0.6,0.05,0.15,0.15]);
+        %     showmesh(node,elem); view(2); xlabel('x'); ylabel('y');
+        
+        
+        
+        ntn = size(elem,1);  %number of triangles after refinement
+        QC = zeros(ntn,4);
+        %     fprintf('coarsen ntn = %g \n',ntn);
+        %     fprintf('size(QR) = %g \n',size(QR));
+        %     fprintf('size(QC) = %g \n',size(QC));
+        %     fprintf('size(tree) = %g \n',size(tree));
+        
         QC = zeros(ntn,4);
         for ii = 1:4
-            QQ = eleminterpolate(QR(:,ii),tree);
-            QC(:,ii) = QQ(1:ntn); 
+            QC(:,ii) = eleminterpolate(QR(:,ii),tree);
+            %         fprintf('size(QC(:,ii)) = %g \n',size(QC(:,ii)));
         end
-        NEPC = eleminterpolate(NEPR,tree);        
+        NEPC = eleminterpolate(NEPR,tree);
         
         % End Coarsen -----------------------------------------------------
-         
+        
         
         
         
@@ -151,52 +185,69 @@ for i = 1:maxIt
         NEP = NEPC;
         % End Coarsening --------------------------------------------------
         
-        
-        % Actual Evolve -------------------------------------------------------
-        
-        [Q, Area] = evolve_step(node,elem,Q,dt,g);
-        t = t+dt;
-        
-        
-        
-        %Plot in colour the results
-        %clf(figure);
-        clf;
-        figure(1);
-        h  = Q(1,:);
-        uh = Q(2,:);
-        u  = uh./h;
-        vh = Q(3,:);
-        v  = vh./h;
-        Ent_after = 0.5*h.*(u.^2+v.^2) + 0.5*g*h.^2; %entropy
-        NEP =(1/dt)*(Ent_after-Q(4,:));%.*Area';
-        %maxNEP = max(abs(NEP));
-        %NEP = NEP/maxNEP;
-        
-        clf(figure(1));
-        figure(1);
-        subplot(1,2,1);
-        showsolution(node,elem,h); view(3); zlim([0. 1.]);
-        xlabel('x'); ylabel('y'); %zlabel('Stage')
-        subplot(1,2,2);
-        showsolution(node,elem,-NEP/max(abs(NEP))); view(3);
-        xlabel('x'); ylabel('y'); pause(0.001);% zlabel('CK/max(|CK|)');
-        
-        clf(figure(2));
-        figure(2);
-        subplot(1,2,1);
-        showsolution(node,elem,uh/max(abs(uh))); view(3); zlim([0. 1.]);
-        xlabel('x'); ylabel('y'); %zlabel('uh/max(|uh|)')
-        subplot(1,2,2);
-        showmesh(node,elem); view(3); xlabel('x'); ylabel('y'); pause(0.001);
+    end
+    
+    % Actual Evolve -------------------------------------------------------
+    
+    [Q, Area, dt] = evolve_step(node,elem,Q,dt,g);
+    
+    
+    tol_Area  = max(Area);
+    t = t+dt;
+    
+    fprintf('TIME %g dt %g \n',t, dt);
+    
+    
+    
+    
+    %Plot in colour the results
+    h  = Q(:,1);
+    uh = Q(:,2);
+    u  = uh./h;
+    vh = Q(:,3);
+    v  = vh./h;
+    Ent_after = 0.5*h.*(u.^2+v.^2) + 0.5*g*h.^2; %entropy
+    NEP =(1/dt)*(Ent_after-Q(:,4));%.*Area';
+    maxNEP = max(abs(NEP));
+    NEP = NEP/maxNEP;
+    
+    fprintf('max h  = %g, min h  = %g \n',max(h),min(h));
+    fprintf('max uh = %g, min uh = %g \n',max(uh),min(uh));
+    
 end
 
+
+clf(figure(3));
+figure(3);
+set(gcf,'Units','normal'); set(gcf,'Position',[0.8,0.55,0.15,0.15]);
+subplot(1,2,1);
+showsolution(node,elem,h); view(3); zlim([0. 1.]);
+xlabel('x'); ylabel('y'); zlabel('Stage');
+
+subplot(1,2,2);
+showsolution(node,elem,-NEP/max(abs(NEP))); view(3);
+xlabel('x'); ylabel('y'); zlabel('NEP/max(|NEP|)');
+
+clf(figure(4));
+figure(4);
+set(gcf,'Units','normal'); set(gcf,'Position',[0.8,0.8,0.15,0.15]);
+subplot(1,2,1);
+showsolution(node,elem,uh/max(abs(uh))); view(3); zlim([0. 1.]);
+xlabel('x'); ylabel('y'); zlabel('uh/max(|uh|)')
+subplot(1,2,2);
+showmesh(node,elem); view(3); xlabel('x'); ylabel('y');
+
+
+
+end
 
 
 
 %% Sub Functions
 
-function [U, Area] = evolve_step(node,elem,Q,dt,g)
+function [U, Area, dt] = evolve_step(node,elem,Q,dt,g)
+
+CFL = 0.5;
 
 % Extract all quantities
 nt = size(elem,1);
@@ -213,12 +264,16 @@ Neighbor = T.neighbor;             %find_neighbor(elem); %neighbor
 Normal   = find_normal(node,elem); %normals
 Edgelength= find_edgelength(node,elem); %edgelength
 Area      = find_area(node,elem);  %areas of triangles
-U = Q;                             %initiation for temporary storage of the quantity
+U = Q; 
+
+%initiation for temporary storage of the quantity
+Flux = zeros(size(Q));
 
 % Evolve
 for j = 1:nt
     Ul = Q(j,:);
     flux = 0.0;
+    max_speed = 0.0;
     for k = 1:3
         neighbor = Neighbor(j,k);
         normal   = Normal(:,k,j);
@@ -226,26 +281,35 @@ for j = 1:nt
         
         Ul_rot = rotate(Ul, normal(1), normal(2));
         Ur = Q(neighbor,:);
-        % Use reflective boundary at the moving water
-        if neighbor==j
-            if Ur(3) ~= 0.0
-                Ur(3) = -Ur(3);
-            end
-        end
         Ur_rot = rotate(Ur, normal(1), normal(2));
         
-        edgeflux  = FluxKNP(Ul_rot,Ur_rot,g);
+        % Use reflective boundary at the moving water
+        if neighbor==j
+            Ur_rot(2) = -Ur_rot(2);      
+%             if Ur(3) ~= 0.0   
+%             end
+        end
+       
+        
+        [edgeflux, ms] = FluxKNP(Ul_rot,Ur_rot,g);
+        
+        max_speed = max(max_speed, ms);
+        %dt = min(dt, CFL*0.5*edgelength/max_speed);
+        
         edgeflux  = rotate(edgeflux, normal(1), -normal(2));
         flux      = flux - edgeflux*edgelength;
     end
     
-    U(j,:) = Ul + dt*flux'/Area(j);
+    
+    Flux(j,:) = flux'/Area(j);
 end
+
+U = Q + dt*Flux;
 end %% -------------------- End of Evolve Step ----------------------------
 
 
 %%
-function F = FluxKNP(Ul,Ur,g)
+function [F, max_speed] = FluxKNP(Ul,Ur,g)
 %dt=1000.0 must return F and dt
 F=zeros(4,1);
 h_l  = Ul(1);     %height
@@ -290,6 +354,8 @@ F(1) = edgeflux_h;
 F(2) = edgeflux_p;
 F(3) = edgeflux_voltrac;
 F(4) = entropy_edgeflux;
+
+
 %return F, dt
 end
 
@@ -297,7 +363,7 @@ end
 function Q = rotate(q, n1, n2)
 % Rotate the momentum component q, that is, q(2) and q(3)
 % from x,y coordinates to coordinates based on normal vector (n1, n2).
-% Result is returned in array 3x1 r
+% Result is returned in array 4x1 r
 % To rotate in opposite direction, call rotate with (q, n1, -n2)
 % Contents of q are changed by this function */
 
